c7f24544bf2821ef4d98bd41ef1196be9916bb0f
Jasper Brumter <jasper.brumter@gmail.com>
2019-07-01 17:45:50 -0400

    feat: initial commit w/ i18n, vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ $t('message') }}
+    <button @click="handleLangSwitch">
+      CHANGE
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  methods: {
+    handleLangSwitch () {
+      this.$i18n.locale = 'fr'
+    },
   }
 }
 </script>
